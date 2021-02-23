f71953831a445421f97896a99f5b6e0d3ba2e299
TunYi Chang <tunyic@uci.edu>
2021-02-22 18:34:53 -0800

    update quiz

---
+++ app/src/main/java/com/example/knowyoursuperhero/Question.java
@@ -0,0 +1,4 @@
+package com.example.knowyoursuperhero;
+
+public class Question {
+}
--- app/src/main/java/com/example/knowyoursuperhero/QuizActivity.java
+++ app/src/main/java/com/example/knowyoursuperhero/QuizActivity.java
@@ -11,17 +11,29 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Button;
+import android.widget.ProgressBar;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class QuizActivity extends AppCompatActivity {
 
+    private int currentQuestion = 1;
+    private List<Question> q_list;
+    private ProgressBar pb ;
+    private TextView tv_progress;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+        pb = findViewById(R.id.progress_bar);
+        pb.setMax(10);
+        tv_progress = findViewById(R.id.tv_progress);
+        tv_progress.setText(currentQuestion+"/10");
         defaultOptionsView();
+
 
 
         final View btnOption1 = findViewById(R.id.tv_option1);
@@ -71,6 +83,59 @@ public class QuizActivity extends AppCompatActivity {
             }
         });
 
+        final View btnNext = findViewById(R.id.button_next);
+        btnNext.setOnClickListener( (v) -> {
+
+            setQuestion();
+
+//                    if (mSelectedOptionPosition == 0) {
+//
+//                        mCurrentPosition++
+//
+//                        when {
+//
+//                            mCurrentPosition <= mQuestionsList!!.size -> {
+//
+//                                setQuestion()
+//                            }
+//                        else -> {
+//
+//                                // TODO (STEP 5: Now remove the toast message and launch the result screen which we have created and also pass the user name and score details to it.)
+//                                // START
+//                                val intent =
+//                                        Intent(this@QuizQuestionsActivity, ResultActivity::class.java)
+//                                intent.putExtra(Constants.USER_NAME, mUserName)
+//                                intent.putExtra(Constants.CORRECT_ANSWERS, mCorrectAnswers)
+//                                intent.putExtra(Constants.TOTAL_QUESTIONS, mQuestionsList!!.size)
+//                                startActivity(intent)
+//                                finish()
+//                                // END
+//                            }
+//                        }
+//                    } else {
+//                        val question = mQuestionsList?.get(mCurrentPosition - 1)
+//
+//                        // This is to check if the answer is wrong
+//                        if (question!!.correctAnswer != mSelectedOptionPosition) {
+//                            answerView(mSelectedOptionPosition, R.drawable.wrong_option_border_bg)
+//                        }
+//                    else {
+//                            mCorrectAnswers++
+//                        }
+//
+//                        // This is for correct answer
+//                        answerView(question.correctAnswer, R.drawable.correct_option_border_bg)
+//
+//                        if (mCurrentPosition == mQuestionsList!!.size) {
+//                            btn_submit.text = "FINISH"
+//                        } else {
+//                            btn_submit.text = "GO TO NEXT QUESTION"
+//                        }
+//
+//                        mSelectedOptionPosition = 0
+//                    }
+                }
+                );
 
 
 
@@ -84,6 +149,31 @@ public class QuizActivity extends AppCompatActivity {
 //        tv_option_three.setOnClickListener(this)
 //        tv_option_four.setOnClickListener(this)
 //        btn_submit.setOnClickListener(this)
+    }
+    private void setQuestion() {
+
+//        Question  currQ =  q_list.get(currentQuestion);// Getting the question from the list with the help of current position.
+        currentQuestion++;
+        defaultOptionsView();
+
+//        if (mCurrentPosition == mQuestionsList!!.size) {
+//            btn_submit.text = "FINISH"
+//        } else {
+//            btn_submit.text = "SUBMIT"
+//        }
+
+        pb.setProgress(currentQuestion);
+        tv_progress.setText(currentQuestion+"/10");
+
+//        progressBar.progress = mCurrentPosition
+//        tv_progress.text = "$mCurrentPosition" + "/" + progressBar.getMax()
+//
+//        tv_question.text = question.question
+//        iv_image.setImageResource(question.image)
+//        tv_option_one.text = question.optionOne
+//        tv_option_two.text = question.optionTwo
+//        tv_option_three.text = question.optionThree
+//        tv_option_four.text = question.optionFour
     }
 
     private void selectedOptionView(TextView tv, int selectedNum) {
