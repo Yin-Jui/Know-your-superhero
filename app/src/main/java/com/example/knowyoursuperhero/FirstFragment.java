2994de69030c4a7aaf964d24f44ec2bdb43ca431
Yin-Jui <ckjimmy634@gmail.com>
2021-02-23 16:45:35 -0800

    Add email verification

--- app/src/main/java/com/example/knowyoursuperhero/FirstFragment.java
+++ app/src/main/java/com/example/knowyoursuperhero/FirstFragment.java
@@ -15,10 +15,18 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class FirstFragment extends Fragment {
+public class FirstFragment extends Fragment implements FirebaseAuth.AuthStateListener {
+
+    TextView user_info;
+    Button verify;
+    int i = 1;
+    String TAG = "FirstFragment";
 
     @Override
     public View onCreateView(
@@ -30,8 +38,47 @@ public class FirstFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_first, container, false);
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.d(TAG, "START");
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if(user != null){
+            user_info.setText("Email: " + user.getEmail() +"/"+ user.isEmailVerified());
+            if(user.isEmailVerified())
+                verify.setVisibility(View.GONE);
+            else
+                verify.setVisibility(View.VISIBLE);
+        }
+        else{
+            user_info.setText("Not login");
+            verify.setVisibility(View.GONE);
+        }
+    }
+
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+
         super.onViewCreated(view, savedInstanceState);
+
+        user_info = view.findViewById(R.id.user_info);
+        verify = view.findViewById(R.id.verify);
+
+
+        verify.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+               FirebaseAuth.getInstance().getCurrentUser().sendEmailVerification().addOnCompleteListener(new OnCompleteListener<Void>() {
+                   @Override
+                   public void onComplete(@NonNull Task<Void> task) {
+                       if (task.isSuccessful()) {
+                           Snackbar.make(view, "Verify email sent", Snackbar.LENGTH_LONG).show();
+                       }
+                   }
+               });
+
+            }
+        });
+
         view.findViewById(R.id.profileButton).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -55,5 +102,12 @@ public class FirstFragment extends Fragment {
             startActivity(intent);
             }
         });
+    }
+
+    @Override
+    public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+        user_info.setText("Email");
+        Log.d(TAG, "AUTH CHANGED");
+
     }
 }
