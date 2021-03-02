04ce38ef5cd57a7244b0c89191b4a18a3a15f9fe
TunYi Chang <tunyic@uci.edu>
2021-03-01 19:00:05 -0800

    finish logic of calculating scores

--- app/src/main/java/com/example/knowyoursuperhero/Contents.java
+++ app/src/main/java/com/example/knowyoursuperhero/Contents.java
@@ -12,16 +12,21 @@ public class Contents {
             "pizza","ramen", "sushi","blood", Question.INTELLIGENCE);
 
     private Question q2 = new Question("Do you work out?",
-            "Always","Sometimes", "Never","Can't",Question.POWER);
+            "Always","Sometimes", "Never","Can't",Question.STRENGTH);
 
     private Question q3 = new Question("How tall are you?",
             "Short","Average", "Tall","Goliath",Question.DURABILITY);
+
+    private Question q4 = new Question("What animal would you describe yourself as?",
+            "Starfish","Turtles", "Rabbits","Leopards",Question.SPEED);
+
 
     public Contents(){
         q_list = new ArrayList<>();
         q_list.add(q1);
         q_list.add(q2);
         q_list.add(q3);
+        q_list.add(q4);
         Q_number = q_list.size();
     }
 
