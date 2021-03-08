10e862851ae02894cd60d503d334bbf38a32d335
Yin-Jui <ckjimmy634@gmail.com>
2021-03-08 13:16:23 -0800

    Add location api

--- app/src/main/java/com/example/knowyoursuperhero/MapsActivity.java
+++ app/src/main/java/com/example/knowyoursuperhero/MapsActivity.java
@@ -12,21 +12,31 @@ import android.os.Bundle;
 import android.util.Log;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
-public class MapsActivity extends FragmentActivity{
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import kotlin.jvm.internal.Ref;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback{
 
     private GoogleMap mMap;
     private FusedLocationProviderClient clientLOC;
     private SupportMapFragment mapFragment;
+    private String TAG = "NearbyUser";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,14 +46,31 @@ public class MapsActivity extends FragmentActivity{
         mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
 
-        //TODO: make sure t user is logged in +
-        // has location associated with them before calling
-        Log.e("********", "ABOUT TO START SERVICE");
+        //Set user's location
+        Utility.Companion.setLocation(new latlng(25, 60));
+        //Get all users' location
+        FirebaseFirestore.getInstance().collection("users")
+                .get()
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        for (QueryDocumentSnapshot document : task.getResult()) {
+                            Users user = document.toObject(Users.class);
+                            Log.d(TAG, user.getUsername()+": "+ user.getLocation().getLatitude() + " " + user.getLocation().getLongitude());
+                        }
+                    } else {
+                        Log.d(TAG, "Error getting documents: ", task.getException());
+                    }
+                });
+
+
+        //TODO: make sure user has location associated with them before service can run
         startService(new Intent(this, GPService.class));
 
+        mapFragment.getMapAsync(this);
+
         //get client location
-        clientLOC = LocationServices.getFusedLocationProviderClient(this);
-        getLocation();
+//        clientLOC = LocationServices.getFusedLocationProviderClient(this);
+//        getLocation();
     }
 
     private void getLocation() {
@@ -57,18 +84,29 @@ public class MapsActivity extends FragmentActivity{
         task.addOnSuccessListener(new OnSuccessListener<Location>() {
             @Override
             public void onSuccess(Location location) {
+                //Log.e("*******", "onSuccess");
                 if (location != null)
+                    //Log.e("*****", "location not null");
+
                     mapFragment.getMapAsync(new OnMapReadyCallback() {
                         @Override
                         public void onMapReady(GoogleMap googleMap) {
-                            LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
-                            //System.out.println("******* found location***");
-                            MarkerOptions options = new MarkerOptions().position(latLng).title("You Are Here");
 
-                            //zoom to point on map
-                            googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10));
-                            //add marker
-                            googleMap.addMarker(options);
+                            // Add a marker in Sydney and move the camera
+                            LatLng sydney = new LatLng(-34, 151);
+                            Utility.Companion.setLocation(new latlng(sydney.latitude, sydney.longitude));
+                            mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+                            mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+//                            LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+//                            MarkerOptions options = new MarkerOptions().position(latLng).title("You Are Here");
+//
+//                            Log.e("******", "lat and long: " + latLng.latitude + "," + latLng.longitude);
+//
+//                            //zoom to point on map
+//                            googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10));
+//                            //add marker
+//                            googleMap.addMarker(options);
                         }
                     });
             }
@@ -92,13 +130,14 @@ public class MapsActivity extends FragmentActivity{
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
-//    @Override
-//    public void onMapReady(GoogleMap googleMap) {
-//        mMap = googleMap;
-//
-//        // Add a marker in Sydney and move the camera
-//        LatLng sydney = new LatLng(-34, 151);
-//        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-//        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-//    }
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        Log.e("******", "onMapReady: " + sydney.longitude + "," +  sydney.latitude );
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
 }
--- app/src/main/java/com/example/knowyoursuperhero/Users.kt
+++ app/src/main/java/com/example/knowyoursuperhero/Users.kt
@@ -1,5 +1,6 @@
 package com.example.knowyoursuperhero
 
-data class Users(val hero:String, var username:String, val imageUrl: String) {
-    constructor(): this("", "", "")
+data class Users(val hero:String, var username:String, val imageUrl: String, var location: latlng) {
+    constructor(): this("", "", "", latlng())
+
 }
