5e90187af9ed7167782e5dfda564086381addb8c
TunYi Chang <tunyic@uci.edu>
2021-03-10 14:19:51 -0800

    hardcode nearby user

--- app/src/main/java/com/example/knowyoursuperhero/MapsActivity.java
+++ app/src/main/java/com/example/knowyoursuperhero/MapsActivity.java
@@ -123,9 +123,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         GoogleMap mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(36.0618483, -115.10159);
+        LatLng sydney = new LatLng(33.59689, -117.658154);
         Log.e("******", "onMapReady: " + sydney.longitude + "," +  sydney.latitude );
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Nearby User"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
