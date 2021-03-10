f4a4d3bbb60509dd1623f7124beae15d447363d9
mezage <mezage@uci.edu>
2021-03-09 23:41:34 -0800

    location now shows on phone, service doesn't get called recursively

--- app/src/main/java/com/example/knowyoursuperhero/GPService.java
+++ app/src/main/java/com/example/knowyoursuperhero/GPService.java
@@ -3,18 +3,32 @@ package com.example.knowyoursuperhero;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
+import android.location.Location;
 import android.os.Handler;
 import android.os.IBinder;
 import android.os.Looper;
 import android.util.Log;
 import android.widget.Toast;
 
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.android.gms.maps.model.LatLng;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.squareup.picasso.Picasso;
+
+import org.jetbrains.annotations.NotNull;
+
+import kotlin.jvm.internal.Intrinsics;
 
 public class GPService extends Service {
     private Context appContext;
+    private String TAG = "NearbyUser";
+    private String uid = "";
+    private double lat = 0, lon = 0;
 
 //    @Override
 //    public int onStartCommand(Intent intent, int flags, int startId) {
@@ -34,31 +48,65 @@ public class GPService extends Service {
     void showToast(){
         if(appContext != null){
             Handler handler = new Handler(Looper.getMainLooper());
-            handler.post(new Runnable() {
+            handler.postDelayed(new Runnable() {
                 @Override
                 public void run() {
+
                     //check to see if user logged in
                     FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-                    if(user != null) {
+                    if (user != null) {
 
-                        //TODO: grab all users location from database
-                        //mDatabase mDatabase = FirebaseDatabase.getInstance().getReference();
+                        //grab logged in user's info
+                        String uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
+                        FirebaseFirestore.getInstance().collection("users").document(uid).get().addOnCompleteListener(new OnCompleteListener() {
+                            public final void onComplete(@NotNull Task task) {
+                                Intrinsics.checkParameterIsNotNull(task, "task");
+                                if (task.isSuccessful()) {
+                                    DocumentSnapshot snapshot = (DocumentSnapshot) task.getResult();
+                                    Users user = snapshot != null ? (Users) snapshot.toObject(Users.class) : null;
+                                    if (user != null) {
+                                        lat = user.getLocation().getLatitude();
+                                        lon = user.getLocation().getLongitude();
+                                    }
+                                }
+                            }
+                        });
 
-                        //user1 is logged-in user, user 2 is everyone else from database
-                        LatLng user1 = new LatLng(32.9697, -96.80322);
-                        LatLng user2 = new LatLng(29.46786, -98.53506);
-                        double distance = distance(user1.latitude, user1.longitude,
-                                user2.latitude, user2.longitude, "M");
+                        //check to see if user even has updated location
+                        if (lat != 0 & lon != 0) {
 
-                        //if distance isn't user(distance == 0) and within certain range
-                        if (distance > 0 & distance < 5)
-                            Toast.makeText(appContext, "User logged in", Toast.LENGTH_SHORT).show();
+                            //Get all users' location
+                            FirebaseFirestore.getInstance().collection("users")
+                                    .get()
+                                    .addOnCompleteListener(task -> {
+                                        if (task.isSuccessful()) {
+                                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                                Users Appuser = document.toObject(Users.class);
+                                                Log.d(TAG, Appuser.getUsername() + ": " +
+                                                        Appuser.getLocation().getLatitude() + " " + Appuser.getLocation().getLongitude());
+                                            }
+                                        } else {
+                                            Log.d(TAG, "Error getting documents: ", task.getException());
+                                        }
+                                    });
+
+//                            //user1 is logged-in user, user 2 is everyone else from database
+//                            LatLng user1 = new LatLng(32.9697, -96.80322);
+//                            LatLng user2 = new LatLng(29.46786, -98.53506);
+//                            double distance = distance(user1.latitude, user1.longitude,
+//                                    user2.latitude, user2.longitude, "M");
+//
+//                            //if distance isn't user(distance == 0) and within certain range
+//                            if (distance > 0 & distance < 5)
+//                                Toast.makeText(appContext, "User logged in", Toast.LENGTH_SHORT).show();
+                        }else
+                            Log.e("*****", "USER still has 0,0");
+
+                        //to make the handler call itself recursively every 6 sec
+                        handler.postDelayed(this, 6000);
                     }
-
-                    //to make the handler call itself recursively every 4.5 sec
-                    handler.postDelayed(this, 4500);
                 }
-            });
+            }, 2000);       //initial call takes 2 sec before running
         }
     }
 
--- app/src/main/java/com/example/knowyoursuperhero/MapsActivity.java
+++ app/src/main/java/com/example/knowyoursuperhero/MapsActivity.java
@@ -10,8 +10,12 @@ import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -33,47 +37,27 @@ import java.util.List;
 
 import kotlin.jvm.internal.Ref;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback{
+public class MapsActivity extends FragmentActivity{
 
     private GoogleMap mMap;
     private FusedLocationProviderClient clientLOC;
     private SupportMapFragment mapFragment;
-    private String TAG = "NearbyUser";
-    private String uid = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
 
-        //Set user's location
+        //Set default location first, maps seems to be less glitchy this wa
         Utility.Companion.setLocation(new latlng(25, 60));
-        //Get all users' location
-        FirebaseFirestore.getInstance().collection("users")
-                .get()
-                .addOnCompleteListener(task -> {
-                    if (task.isSuccessful()) {
-                        for (QueryDocumentSnapshot document : task.getResult()) {
-                            Users user = document.toObject(Users.class);
-                            Log.d(TAG, user.getUsername()+": "+ user.getLocation().getLatitude() + " " + user.getLocation().getLongitude());
-                        }
-                    } else {
-                        Log.d(TAG, "Error getting documents: ", task.getException());
-                    }
-                });
-
-
-        //TODO: make sure user has location associated with them before service can run
-        startService(new Intent(this, GPService.class));
-
-        mapFragment.getMapAsync(this);
 
         //get client location
-//        clientLOC = LocationServices.getFusedLocationProviderClient(this);
-//        getLocation();
+        clientLOC = LocationServices.getFusedLocationProviderClient(this);
+        getLocation();
     }
 
     private void getLocation() {
@@ -81,26 +65,46 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
                 ActivityCompat.checkSelfPermission(this,
                         Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 44);
-        }
-        Task<Location> task = clientLOC.getLastLocation();
-        task.addOnSuccessListener(new OnSuccessListener<Location>() {
-            @Override
-            public void onSuccess(Location location) {
-                //Log.e("*******", "onSuccess");
-                if (location != null)
-                    //Log.e("*****", "location not null");
 
-                    mapFragment.getMapAsync(new OnMapReadyCallback() {
+            //ask for permission if not initially given it
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION}, 44);
+        }
+
+        //run service only when user gives location permissions
+        startService(new Intent(this, GPService.class));
+
+        clientLOC.getLastLocation().addOnSuccessListener(this, location -> {
+            if (location != null) {
+                mapFragment.getMapAsync(new OnMapReadyCallback() {
                         @Override
                         public void onMapReady(GoogleMap googleMap) {
+                            LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+                            MarkerOptions options = new MarkerOptions().position(latLng).title("You Are Here");
 
-                            // Add a marker in Sydney and move the camera
-                            LatLng sydney = new LatLng(-34, 151);
-                            Utility.Companion.setLocation(new latlng(sydney.latitude, sydney.longitude));
-                            mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-                            mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+                            //Log.e("******", "lat and long: " + latLng.latitude + "," + latLng.longitude);
 
+                            //zoom to point on map
+                            googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10));
+                            //add marker
+                            googleMap.addMarker(options);
+                        }
+                    });
+            }
+        });
+
+
+//        Task<Location> task = clientLOC.getLastLocation();
+//        task.addOnSuccessListener(new OnSuccessListener<Location>() {
+//            @Override
+//            public void onSuccess(Location location) {
+//                //Log.e("*******", "onSuccess");
+//                if (location != null)
+//                    //Log.e("*****", "location not null");
+//
+//                    mapFragment.getMapAsync(new OnMapReadyCallback() {
+//                        @Override
+//                        public void onMapReady(GoogleMap googleMap) {
 //                            LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
 //                            MarkerOptions options = new MarkerOptions().position(latLng).title("You Are Here");
 //
@@ -110,37 +114,31 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 //                            googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10));
 //                            //add marker
 //                            googleMap.addMarker(options);
-                        }
-                    });
-            }
-        });
+//                        }
+//                    });
+//            }
+//        });
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         if (requestCode == 44){
             if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
                 getLocation();
+            else
+                Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show();
         }
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
-
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        Log.e("******", "onMapReady: " + sydney.longitude + "," +  sydney.latitude );
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-    }
+//    @Override
+//    public void onMapReady(GoogleMap googleMap) {
+//        mMap = googleMap;
+//
+//        // Add a marker in Sydney and move the camera
+//        LatLng sydney = new LatLng(-34, 151);
+//        Log.e("******", "onMapReady: " + sydney.longitude + "," +  sydney.latitude );
+//        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+//        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+//    }
 }
